eb454edc15bd9ea03fa60fa342056fb2626dc52e
ozan balcı <ozanbalcieem@gmail.com>
2024-01-30 17:09:54 +0300

    -

--- src/app/pages/applications/customer/components/AddCustomerForm.jsx
+++ src/app/pages/applications/customer/components/AddCustomerForm.jsx
@@ -14,18 +14,18 @@ export default function AddCustomerForm() {
   const { setUserData, userRoles, userStatus} = useCustomerContext(CustomerContext);
   
 
-  const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
-  // const userSchema = Yup.object().shape({
-  //   password: Yup.string()
-  //     .matches(passwordRules, { message: "Please create a stronger password" })
-  //     .required("Required"),
-  //   confirmPassword: Yup.string()
-  //     .oneOf([Yup.ref("password"), null], "Passwords must match")
-  //     .required("Required"),
-  // });
+  const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/
+  const customerSchema = Yup.object().shape({
+    password: Yup.string()
+      .matches(passwordRules, {message: 'Please create a stronger password'})
+      .required('Required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Required'),
+  })
 
-  const { handleSubmit, handleChange, values } = useFormik({
-    // validationSchema: userSchema,
+  const { handleSubmit, handleChange, values, touched, errors, getFieldProps } = useFormik({
+    validationSchema: customerSchema,
     initialValues: {
       name: "",
       surname: "",
@@ -151,6 +151,64 @@ export default function AddCustomerForm() {
               value={values.status}
               options={[{label: 'Empty', value: null}, ...userStatus]}
             />
+          </div>
+
+          <div className="col-span-">
+            <label className="fw-bold  mb-2 text-gray-300 ">Password</label>
+          </div>
+          <div className="col-span-5">
+          <Input
+              type='password'
+              name='password'
+              placeholder='Password'
+              onChange={handleChange}
+              value={values.password}
+              autoComplete='off'
+              {...getFieldProps('password')}
+              className={`form-control form-control-solid  ${
+                touched.password && errors.password
+                  ? 'is-invalid'
+                  : touched.password && !errors.password
+                  ? 'is-valid'
+                  : ''
+              }`}
+            />
+             {touched.password && errors.password && (
+              <div className='text-red-300 '>
+                <div className='fv-help-block'>
+                  <span role='alert'>{errors.password}</span>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="col-span-1">
+            <label className="fw-bold  mb-2 text-gray-300 ">Password Confirmation</label>
+          </div>
+          <div className="col-span-5">
+          <Input
+              type='password'
+              name='confirmPassword'
+              placeholder='Password Confirmation'
+              onChange={handleChange}
+              value={values.confirmPassword}
+              autoComplete='off'
+              {...getFieldProps('confirmPassword')}
+              className={`form-control form-control-solid  ${
+                touched.confirmPassword && errors.confirmPassword
+                  ? 'is-invalid'
+                  : touched.confirmPassword && !errors.confirmPassword
+                  ? 'is-valid'
+                  : ''
+              }`}
+            />
+             {touched.confirmPassword && errors.confirmPassword && (
+              <div className='fv-plugins-message-container'>
+                <div className='fv-help-block text-red-300'>
+                  <span role='alert'>{errors.confirmPassword}</span>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
